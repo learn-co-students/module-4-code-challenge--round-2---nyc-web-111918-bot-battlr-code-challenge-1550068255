75d3a9a96fed8b91b7390bd4a47a27e50b4ac25e
gavinmoore36 <gavizoid16@gmail.com>
2019-02-13 10:45:29 -0500

    Done and moving onto bonus

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,13 +1,16 @@
 import React from "react";
 import BotCollection from './BotCollection'
 import YourBotArmy from './YourBotArmy'
+import BotSpecs from './../components/BotSpecs'
 
 const botAPI = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
   state = {
     bots: [],
-    army: []
+    army: [],
+    clicked: false,
+    foundBot: {}
   }
 
   componentDidMount() {
@@ -39,11 +42,40 @@ class BotsPage extends React.Component {
     })
   }
 
+  handleClick = (id) => {
+    const foundBot = this.state.bots.find(bot=> bot.id === id)
+    this.setState({
+      foundBot: foundBot,
+      clicked: !this.state.clicked
+    })
+
+  }
+
+  displaySettings = () => {
+    if (this.state.clicked === false) {
+      return <BotCollection handleClick={this.handleClick} bots={this.state.bots}/>
+    } else {
+      return <BotSpecs goBack={this.handleClick} enlistClick={this.handleAddClick} bot={this.state.foundBot} />
+    }
+  }
+
+ // displaySettings = (event) => {
+ //   if (event.target.value === 'Go Back') {
+ //     console.log('Go Back');
+ //   } else if (event.target.value === 'Enlist') {
+ //     console.log('Enlist clicked');
+ //   }
+ //   else {
+ //     console.log('image clicked for' );
+ //   }
+ // }
+
+
   render() {
     return (
       <div>
         <YourBotArmy handleClick={this.handleRemovalClick} army={this.state.army} />
-        <BotCollection handleClick={this.handleAddClick} bots={this.state.bots}/>
+        {this.displaySettings()}
       </div>
     );
   }
