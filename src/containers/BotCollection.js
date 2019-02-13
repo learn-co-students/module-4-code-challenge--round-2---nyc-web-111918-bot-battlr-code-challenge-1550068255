f2cbc834215ad8227c72ae9f8fbdac84b9ee506d
dsound-zz <demiansims@gmail.com>
2019-02-13 10:49:01 -0500

    mission accomplished deliverables

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,16 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
 
   render(){
+    console.log(this.props);
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+        Collection of all bots
+    		  {this.props.bots.map(bot => {
+            return <BotCard key={bot.id} bot={bot} botClick={this.props.botClick} />
+          })}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,34 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state = { allBots: [],
+            botArmy: [] }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(r => r.json())
+    .then(bots => {
+      this.setState({ allBots: bots })
+    })
+  }
+
+  botClick = (botId) => {
+    if (!this.state.botArmy.find(bot => bot.id === botId)) {
+    const selectedBot = this.state.allBots.find(bot => bot.id === botId)
+    this.setState({ botArmy: [...this.state.botArmy, selectedBot] })
+  }
+}
+
 
   render() {
+    console.log(this.state.botArmy)
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botArmy}/>
+
+        <BotCollection bots={this.state.allBots} botClick={this.botClick} />
       </div>
     );
   }
